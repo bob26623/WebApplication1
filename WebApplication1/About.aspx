9eb11223c5066a468dbe1321d6b2a912806c9503
Alice <alice@csyautopart.com>
2023-09-13 00:42:36 +0000

    Initial commit

---
+++ WebApplication1/About.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="WebApplication1.About" Async="true" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <main aria-labelledby="title">
+        <h2 id="title"><%: Title %>.</h2>
+        <h3>Your application description page.</h3>
+        <p>Use this area to provide additional information.</p>
+        <ul class="navbar-nav navbar-right">
+            <li><asp:TextBox id="message" runat="server" Text="Message Here" /></li>
+            <li><asp:LinkButton Text="Send Mail" runat="server" CssClass="nav-item" OnClick="SendMail_Click" /></li>
+        </ul>
+    </main>
+</asp:Content>
---
+++ WebApplication1/About.aspx.cs
@@ -0,0 +1,68 @@
+﻿using Azure.Identity;
+using Microsoft.Graph.Models;
+using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.Graph.Users.Item.SendMail;
+using System.Configuration;
+
+
+namespace WebApplication1
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected async void SendMail_Click(object sender, EventArgs e)
+        {
+            var scopes = new[] { "https://graph.microsoft.com/.default" };
+
+            // Values from app registration
+            var clientId = ConfigurationManager.AppSettings["graph:ClientId"];
+            var tenantId = ConfigurationManager.AppSettings["graph:TenantId"];
+            var clientSecret = ConfigurationManager.AppSettings["graph:ClientSecret"];
+            var senderId = ConfigurationManager.AppSettings["graph:SenderId"];
+
+            // using Azure.Identity;
+            var options = new ClientSecretCredentialOptions
+            {
+                AuthorityHost = AzureAuthorityHosts.AzurePublicCloud,
+            };
+
+            var clientSecretCredential = new ClientSecretCredential(tenantId, clientId, clientSecret, options);
+
+            var graphClient = new GraphServiceClient(clientSecretCredential, scopes);
+            var requestBody = new SendMailPostRequestBody
+            {
+                Message = new Message
+                {
+                    Subject = "Example email testing",
+                    Body = new ItemBody
+                    {
+                        ContentType = BodyType.Text,
+                        Content = message.Text,
+                    },
+                    ToRecipients = new List<Recipient>
+                    {
+                        new Recipient
+                        {
+                            EmailAddress = new EmailAddress
+                            {
+                                Address = Context.User.Identity.Name,
+                            },
+                        },
+                    },
+                },
+                SaveToSentItems = false,
+            };
+            await graphClient.Users[senderId].SendMail.PostAsync(requestBody);
+        }
+    }
+}
